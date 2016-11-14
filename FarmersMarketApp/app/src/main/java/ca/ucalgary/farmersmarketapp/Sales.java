49fa262a851051c62533a06ee63e08b98e33b9e7
Whoosp <gjusdhj@gmail.com>
2016-11-14 12:15:15 -0700

    initial setup of app and buttons
    The first button on the main screen is not working properly

---
+++ FarmersMarketApp/app/src/main/java/ca/ucalgary/farmersmarketapp/Sales.java
@@ -0,0 +1,18 @@
+package ca.ucalgary.farmersmarketapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Whoosp on 2016-11-14.
+ */
+public class Sales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.people_screen);
+    }
+
+}
